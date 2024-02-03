e02dd7593cb83a2e9ce69e73be2b9821a0ca02be
zipmic <mail@code.dk>
2024-02-03 11:29:42 +0100

    control

--- Assets/TestScene/TestInputSystem.cs
+++ Assets/TestScene/TestInputSystem.cs
@@ -9,21 +9,22 @@ public class TestInputSystem : MonoBehaviour
 	private PlayerInput _playerInput;
 	private PlayerInputActions _playerInputActions;
 
+	public Transform playersprite;
+
 	private void Awake()
 	{
 		_rb = GetComponent<Rigidbody2D>();
 		_playerInputActions = new PlayerInputActions();
 		_playerInputActions.Player.Enable();
 		_playerInputActions.Player.Jump.performed += Jump;
-	
-
 	}
 
 	private void Update()
 	{
 		Vector2 inputVector = _playerInputActions.Player.Movement.ReadValue<Vector2>();
 		float speed = 5f;
-		_rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed);
+		_rb.velocity = new Vector3(inputVector.x,inputVector.y,0) * speed;
+		playersprite.up = _rb.velocity.normalized * -1;
 	}
 
 	public void Jump(InputAction.CallbackContext context)
@@ -31,7 +32,7 @@ public class TestInputSystem : MonoBehaviour
         //print("Jump!" + context.phase);
 		if (context.performed)
 		{
-			_rb.AddForce(Vector3.up * 5f);
+			
 		}
     }
 
